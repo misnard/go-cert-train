5d892e249a38aa67e88aa648de79920a6e677571
Maher Isnard <maher.isnard@gmail.com>
2021-07-28 15:46:47 +0200

    Add cli and csv parser wip

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go-cert-train/cert"
+	"go-cert-train/html"
 	"go-cert-train/pdf"
 	"os"
 )
 
 func main() {
-	c, err := cert.New("Golang programming", "Bob Dylan", "2021-06-21")
+	outputType := flag.String("type", "pdf", "Output type of the certificate.")
+	inputFileName := flag.String("file", "", "Input data source file.")
+	flag.Parse()
+
+	var saver cert.Saver
+	var err error
+
+	switch *outputType {
+		case "html":
+			saver, err = html.New("output")
+		case "pdf":
+			saver, err = pdf.New("output")
+		default:
+			fmt.Printf("Unknown output type. got=%v\n", *outputType)
+	}
+
 	if err != nil {
-		fmt.Printf("Error during certificate creation: %v", err)
+		fmt.Printf("Could not create generator: %v", err)
 		os.Exit(1)
 	}
 
-	var saver cert.Saver
-	saver, err = pdf.New("output")
+	c, err := cert.New("Golang programming", "Bob Dylan", "2021-06-21")
 	if err != nil {
-		fmt.Printf("Error during pdf generator creation: %v", err)
+		fmt.Printf("Error during certificate creation: %v", err)
 		os.Exit(1)
 	}
 
